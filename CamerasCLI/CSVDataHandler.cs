1bceb194361a0ad2768ce3580d439f87f3de12f5
infinicky <nickyw.schaafsma@gmail.com>
2023-02-28 19:44:12 +0100

    Fixed CLI functionality

--- CamerasCLI/CSVDataHandler.cs
+++ CamerasCLI/CSVDataHandler.cs
@@ -24,7 +24,7 @@ namespace InfiNickyCodes
             csv_file_path = PATH;
             Cameras = new List<Camera>();
             //CreateCamerasFromCSV();
-            //Cameras = CreateCamerasFromCSV();
+            Cameras = CreateCamerasFromCSV();
         }
 
         /// <summary>
